073209304101015fa8f6446b1df8ff2e9a078a2d
Allan Zheng <58377006+azheng1991@users.noreply.github.com>
2020-07-02 13:36:22 -0400

    Task 1.3 Completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,13 @@
 
-var pokemonList = [];
 var pokemonList = [
 { name: 'bulbasaur', height: 0.7, types: ['grass', 'poison']},
-{ name: 'charmander', height: 0.6, types: ['fire']}
+{ name: 'charmander', height: 0.6, types: ['fire']},
 { name: 'squirtle', height: 0.5, types: ['water']}
 ]
 
 for (var i=0; i<pokemonList.length; i++) {
-document.write(pokemonList.name+"(pokemonList.height)")
-
-//check to see if the pokemon has more than one types
-if (types[i]>1) {
-  document.write('Wow! That's a lot of types!')
-}
-
+  document.write('<h2>' + pokemonList[i].name + '</h2>' + ' Height: ' + pokemonList[i].height)
+  if (pokemonList[i].types.length > 1) {
+    document.write('<h3>' + 'Wow, that\'s a lot of types!' + '</h3>')
+    }
+  }
