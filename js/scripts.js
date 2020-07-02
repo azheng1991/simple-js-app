63367f75eb8c32efaac455394e7ce9486ace921e
Allan Zheng <58377006+azheng1991@users.noreply.github.com>
2020-07-02 01:38:07 -0400

    First Attempt at For Loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,6 @@ var pokemonList = [
   { name: 'charmander', height: 0.6, types: ['fire']}
   { name: 'squirtle', height: 0.5, types: ['water']}
 ]
-
+for (var i=0; i<pokemonList.length; i++) {
+  document.write(name+"(height)")
       
