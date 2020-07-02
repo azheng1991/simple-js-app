92b565d3a41712ee3abc664fc34077e5fbec3e94
Allan Zheng <58377006+azheng1991@users.noreply.github.com>
2020-07-02 00:42:55 -0400

    Updated Syntax

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 var pokemonList = [];
 var pokemonList = [
   { name: 'bulbasaur', height: 0.7, types: ['grass', 'poison']},
-  { name: 'charmander', height: 0.6, types: [fire]}
-  { name: 'squirtle', height: 0.5, types: [water]}
-}
+  { name: 'charmander', height: 0.6, types: ['fire']}
+  { name: 'squirtle', height: 0.5, types: ['water']}
+]
 
       
