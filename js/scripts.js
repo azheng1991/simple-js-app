1859d3279e8069ed0474b38b0a4502c22020d8ce
Allan Zheng <58377006+azheng1991@users.noreply.github.com>
2020-07-02 15:09:40 -0400

    Complete Ex. 1.4

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,10 @@ var pokemonList = [
 { name: 'squirtle', height: 0.5, types: ['water']}
 ]
 
-for (var i=0; i<pokemonList.length; i++) {
-  document.write('<h2>' + pokemonList[i].name + '</h2>' + ' Height: ' + pokemonList[i].height)
-  if (pokemonList[i].types.length > 1) {
-    document.write('<h3>' + 'Wow, that\'s a lot of types!' + '</h3>')
-    }
+
+pokemonList.forEach(function(pokemon) {
+  document.write(`<h2>${pokemonList[i].name}</h2> Height: ${pokemonList[i].height}`);
+if (pokemonList[i].types.length > 1) {
+  document.write('<h3>' + 'Wow, that\'s a lot of types!' + '</h3>')
   }
+}
