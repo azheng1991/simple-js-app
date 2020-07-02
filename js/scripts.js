32a99356de50e32f1267a43d9ec6c3fd148f1d8c
Allan Zheng <58377006+azheng1991@users.noreply.github.com>
2020-07-02 00:40:48 -0400

    Declared a pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-alert('Hello world');
-var favoriteFood = 'calamari';
-document.write(favoriteFood)
+var pokemonList = [];
+var pokemonList = [
+  { name: 'bulbasaur', height: 0.7, types: ['grass', 'poison']},
+  { name: 'charmander', height: 0.6, types: [fire]}
+  { name: 'squirtle', height: 0.5, types: [water]}
+}
+
+      
