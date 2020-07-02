a78b316c0b42576dd76554f288f13d8b47941099
Allan Zheng <58377006+azheng1991@users.noreply.github.com>
2020-07-02 16:56:53 -0400

    Completed Ex. 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 
-var pokemonList = [
+var pokemonRepository = (function () {
+  var pokemonList = [
 { name: 'bulbasaur', height: 0.7, types: ['grass', 'poison']},
 { name: 'charmander', height: 0.6, types: ['fire']},
 { name: 'squirtle', height: 0.5, types: ['water']}
 ]
+  return {
+  add: function(pokemon) {
+    pokemonList.push(pokemon);
+  },
+  getAll: function() {
+    return pokemonList;
+  }
+  };
+  })();
 
-
-pokemonList.forEach(function(pokemonList) {
+pokemonRepository.getAll().forEach(function(pokemonList) {
   document.write('<h2>' + pokemonList.name + '</h2>' + ' Height: ' + pokemonList.height);
 });
