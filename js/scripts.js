a2ae987086c76587e501d3fbc9bb9bd7d2534834
Allan Zheng <58377006+azheng1991@users.noreply.github.com>
2020-07-02 22:23:23 -0400

    Completed Ex. 1.6

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,49 @@
+var pokemonRepository = (function() {    //Start of IIFE
+  var repository = [
+  { name: 'bulbasaur', height: 0.7, types: ['grass', 'poison']},
+  { name: 'charmander', height: 0.6, types: ['fire']},
+  { name: 'squirtle', height: 0.5, types: ['water']}
+  ];
 
-var pokemonRepository = (function () {
-  var pokemonList = [
-{ name: 'bulbasaur', height: 0.7, types: ['grass', 'poison']},
-{ name: 'charmander', height: 0.6, types: ['fire']},
-{ name: 'squirtle', height: 0.5, types: ['water']}
-]
-  return {
-  add: function(pokemon) {
-    pokemonList.push(pokemon);
-  },
-  getAll: function() {
-    return pokemonList;
+  var $pokemonList = document.querySelector('ul');
+
+  //Function to add new Pokemon data
+  function add(pokemon) {
+    repository.push(pokemon);
+    }
+
+
+  //Function to pull all Pokemon data
+  function getAll() {
+    return repository;
   }
-  };
-  })();
 
-pokemonRepository.getAll().forEach(function(pokemonList) {
-  document.write('<h2>' + pokemonList.name + '</h2>' + ' Height: ' + pokemonList.height);
+  //Function to add list for each pokemon object
+  function addListItem(pokemon) {
+    var listItem = document.createElement('li');
+    var button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-name');
+    listItem.appendChild(button);
+    $pokemonList.appendChild(listItem)
+    button.addEventListener('click', function() {
+      showDetails(pokemon)
+    })
+  }
+
+   //Function to show details of each Pokemon
+   function showDetails(pokemon) {
+      console.log(pokemon)
+   }
+
+  return{
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails,
+  };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.addListItem(pokemon);
 });
